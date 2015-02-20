0e708e27b2b7def62ef5e0148fcd4788ca659d32
Alessandro Menezes <alessandromenezes22@gmail.com>
2015-02-20 11:15:50 -0300

    Código do problema PRIMO

--- PRIMO.rb
+++ PRIMO.rb
@@ -1,1 +1,26 @@
+
+def primo(n) 
+    if n == 0 || n ==1
+    	return false
+    elsif n == 2 || n == 3
+    	return true
+    elsif 
+        raiz = Math.sqrt(n).to_i
+        for i in 2..raiz 
+            if n%i == 0 
+                return false
+            end
+        end
+    end
+    return true 
+end 
+
 n = gets.chomp.to_i 
+
+isprime = primo(n)
+if isprime
+	puts "sim"
+else 
+	puts "nao"
+end
+
